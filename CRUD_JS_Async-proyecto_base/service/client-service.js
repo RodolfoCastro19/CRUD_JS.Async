89730b274c2c20af75d42d183e919e6838382115
Rodolfo <rodolfocastro706@gmail.com>
2023-01-25 20:37:54 -0300

    clase03 terminada

--- CRUD_JS_Async-proyecto_base/service/client-service.js
+++ CRUD_JS_Async-proyecto_base/service/client-service.js
@@ -2,9 +2,26 @@
 //Fetch API
 const listaClientes = ( ) => fetch('http://localhost:3000/perfil').then(respuesta => respuesta.json());
 
+const crearCliente = (nombre, email) => {
+  return fetch('http://localhost:3000/perfil', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({nombre, email, id: uuid.v4()})
+  }) 
+};
+
+const eliminarCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: 'DELETE',
+  })
+}
 export const clientServices = {
   listaClientes,
-}
+  crearCliente,
+  eliminarCliente
+};
 
 
 
